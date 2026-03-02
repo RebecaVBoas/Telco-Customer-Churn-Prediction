d804845e6b9470f431576e5aeec206e260a4826e
RebecaVBoas <rebecavilasboasdejesus1@gmail.com>
2026-03-02 14:42:09 -0300

    feat: refatoração do pipiline

--- src/data_prep.py
+++ src/data_prep.py
@@ -1,60 +1,85 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin
+import sklearn
 
-def preprocess_basic(df):
-    
-    df = df.copy()
-    
-    cols_to_drop = ['customerID', 'gender', 'TotalCharges']
-    df = df.drop(columns=[c for c in cols_to_drop if c in df.columns], errors='ignore')
-    
-    df = df.rename(columns={'tenure': 'Tenure'})
-    
-    retention_services = ['TechSupport', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection']
-    if all(col in df.columns for col in retention_services):
-        df['Security_Svc_Count'] = (df[retention_services] == 'Yes').sum(axis=1)
-        df.drop(columns=retention_services, inplace=True)
+sklearn.set_config(transform_output="pandas")
+
+
+class BasicDataCleaner(BaseEstimator, TransformerMixin):
+    """
+    Realiza a limpeza inicial e a criação de features que NÃO dependem 
+    de estatísticas dos dados de treino (ex: não usa média, quantil, etc).
+    """
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        df = X.copy() 
         
-    binary_features = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 
-                       'MultipleLines', 'StreamingTV', 'StreamingMovies', 'SeniorCitizen', 'Churn']
-    mapping = {'Yes': 1, 'No': 0, 'No internet service': 0, 'No phone service': 0}
-    
-    for col in binary_features:
-        if col in df.columns:
-            df[col] = df[col].map(mapping).fillna(0).astype(int)
+        cols_to_drop = ['customerID', 'gender', 'TotalCharges']
+        df = df.drop(columns=[c for c in cols_to_drop if c in df.columns], errors='ignore')
+        
+        df = df.rename(columns={'tenure': 'Tenure'})
+        
+        
+        retention_services = ['TechSupport', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection']
+        existing_services = [col for col in retention_services if col in df.columns]
+        
+        if len(existing_services) == len(retention_services):
+            df['Security_Svc_Count'] = (df[existing_services] == 'Yes').sum(axis=1)
+            df = df.drop(columns=existing_services)
             
-    return df
+        
+        binary_features = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 
+                           'MultipleLines', 'StreamingTV', 'StreamingMovies']
+        mapping = {'Yes': 1, 'No': 0, 'No internet service': 0, 'No phone service': 0}
+        
+        for col in binary_features:
+            if col in df.columns:
+                df[col] = df[col].map(mapping).fillna(0).astype(int)
+                
+        return df
 
-class ChurnTransformer:
-   
+
+class PriceShockTransformer(BaseEstimator, TransformerMixin):
+    """
+    Cria a feature Is_Price_Shock. Como ela depende de um quantil, 
+    PRECISAMOS calcular isso no fit (dados de treino) e aplicar no transform.
+    """
     def __init__(self):
-        self.scaler = StandardScaler()
-        self.encoder = OneHotEncoder(drop='first', sparse_output=False, handle_unknown='ignore')
         self.limiar_faturamento = None
-        self.one_hot_features = ['Contract', 'PaymentMethod', 'InternetService']
-        self.numerical_features = ['Tenure', 'MonthlyCharges', 'Security_Svc_Count']
 
-    def fit(self, df_train):
-        self.limiar_faturamento = df_train['MonthlyCharges'].quantile(0.80)
-        
-        self.encoder.fit(df_train[self.one_hot_features])
-        
-        self.scaler.fit(df_train[self.numerical_features])
+    def fit(self, X, y=None):
+       
+        self.limiar_faturamento = X['MonthlyCharges'].quantile(0.80)
+        return self
 
-    def transform(self, df):
-        df = df.copy()
+    def transform(self, X):
+        df = X.copy()
         
         df['Is_Price_Shock'] = (df['MonthlyCharges'] > self.limiar_faturamento).astype(int)
-        
-        encoded_data = self.encoder.transform(df[self.one_hot_features])
-        encoded_cols = self.encoder.get_feature_names_out(self.one_hot_features)
-        df_encoded = pd.DataFrame(encoded_data, columns=encoded_cols, index=df.index)
-        
-        df = pd.concat([df.drop(self.one_hot_features, axis=1), df_encoded], axis=1)
-        
-        
-        df[self.numerical_features] = self.scaler.transform(df[self.numerical_features])
-        
         return df
+
+
+one_hot_cols = ['Contract', 'PaymentMethod', 'InternetService']
+num_cols = ['Tenure', 'MonthlyCharges', 'Security_Svc_Count'] 
+
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), num_cols),
+        ('cat', OneHotEncoder(drop='first', sparse_output=False, handle_unknown='ignore'), one_hot_cols)
+    ],
+    remainder='passthrough' 
+)
+
+
+meu_pipeline = Pipeline(steps=[
+    ('cleaner', BasicDataCleaner()),
+    ('price_shock', PriceShockTransformer()),
+    ('preprocessor', preprocessor)
+])
